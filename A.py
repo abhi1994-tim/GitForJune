06d48de8565e0734f9308db1d9581398d16d046e
Abhijeet Kumar Gupta <88642995+abhi1994-tim@users.noreply.github.com>
2024-06-25 09:37:59 +0530

    Update A.py

--- A.py
+++ A.py
@@ -3,3 +3,5 @@ print("this is just sample to check in Git, tracking")
 print("yes i am tracking you, go and check in the GIT, i  am reflectig there")
 
 print("Yayy, i have gained one more code add to this in the folder")
+
+print("this is code written in Github, Practicing for fetch")
