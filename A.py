dcf3febabd89e832814e2e17be9d89a676bbdc9e
Abhijeet Kumar Gupta <abhijeet0920@gmail.com>
2024-06-25 09:56:41 +0530

    seconf_update

--- A.py
+++ A.py
@@ -5,3 +5,5 @@ print("yes i am tracking you, go and check in the GIT, i  am reflectig there")
 print("Yayy, i have gained one more code add to this in the folder")
 
 print("this is code written in Github, Practicing for fetch")
+
+print("once Again")
